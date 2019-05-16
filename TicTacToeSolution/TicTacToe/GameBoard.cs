27e277167647627388b94d439f2eeafd367f37bb
MartijnDevGron <luub_82@hotmail.com>
2019-05-16 10:34:19 +0200

    added the class GameBoard

---
+++ TicTacToeSolution/TicTacToe/GameBoard.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class GameBoard
+    {
+        private int[,] map;
+
+
+        public GameBoard()
+        {
+            map = new int[3, 3];
+        }
+
+        public int[,] GetMap()
+        {
+            return map;
+        }
+
+        public void PrintMap(int[,] map)
+        {
+            Console.WriteLine(" 123");
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                Console.Write(i + 1);
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (map[j, i] == 0) Console.Write(" ");
+                    if (map[j, i] == 1) Console.Write("X");
+                    if (map[j, i] == 2) Console.Write("O");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private bool WinRow(int[,] map)
+        {
+            bool ret = false;
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                if (map[i, 0] != 0)
+                {
+                    if (map[i, 0] == map[i, 1] && map[i, 1] == map[i, 2])
+                        ret = true;
+                }
+            }
+            return ret;
+        }
+
+        private bool CheckThreeInColumn(int[,] map, int column)
+        {
+            return (map[0, column] != 0 && map[0, column] == map[1, column] && map[1, column] == map[2, column]);
+        }
+
+        private bool WinColumn(int[,] map)
+        {
+            return (CheckThreeInColumn(map, 0) ||
+                 CheckThreeInColumn(map, 1) ||
+                 CheckThreeInColumn(map, 2));
+        }
+
+        private bool WinDiagonal(int[,] map)
+        {
+
+            return ((map[1, 1] != 0 && map[0, 0] == map[1, 1] && map[1, 1] == map[2, 2]) ||
+                (map[1, 1] != 0 && map[0, 2] == map[1, 1] && map[1, 1] == map[2, 0]));
+        }
+
+        public bool WinGame(int [,] map)
+        {
+            return (WinRow (map) || WinColumn(map) || WinDiagonal(map));
+        }
+
+        
+        public string CheckWhoWins(int[,] map, string player1, string player2)
+        {
+            if (WinRow(map))
+            {
+                if ((map[0, 0] == 1 && map[0, 1] == 1 && map[0, 2] == 1) ||
+                    (map[1, 0] == 1 && map[1, 1] == 1 && map[1, 2] == 1) ||
+                    (map[2, 0] == 1 && map[2, 1] == 1 && map[2, 2] == 1))
+                {
+                    return player1;
+                }
+                else return player2;
+            }
+            if (WinColumn(map))
+            {
+                if ((map[0, 0] == 1 && map[1, 0] == 1 && map[2, 0] == 1) ||
+                    (map[0, 1] == 1 && map[1, 1] == 1 && map[2, 1] == 1) ||
+                    (map[0, 2] == 1) && map[1, 2] == 1 && map[2, 2] == 1)
+                {
+                    return player1;
+                }
+                else return player2;
+            }
+            if (WinDiagonal(map))
+            {
+                if (map[1, 1] == 1) return player1;
+                else return player2;
+            }
+            return "";
+        }
+
+        //method for check if field is full
+        public bool MapFull(int[,] map)
+        {
+            
+            int emptySpotCount = 0;
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (map[i, j] == 0)
+                    {
+                        emptySpotCount++;
+                    }
+                }
+            }
+            return emptySpotCount == 0;
+        }
+    }
+}
--- TicTacToeSolution/TicTacToe/Program.cs
+++ TicTacToeSolution/TicTacToe/Program.cs
@@ -11,90 +11,41 @@ namespace TicTacToe
         static void Main(string[] args)
         {
             string player1, player2;
+            var gameBoard = new GameBoard();
             Intro(out player1, out player2);
 
-            // print the map
-            int[,] map = new int[3, 3];
-            Mapgrid(map);
+            int[,] map = gameBoard.GetMap();
+            gameBoard.PrintMap(map);
+            string winner;
 
-            //Turn player 1
-            while (!WinRow(map) && !WinColumn(map) && !WinDiagonal(map) && !MapFull(map))
+            while (!gameBoard.WinGame(map) && !gameBoard.MapFull(map))
             {
-                PrintTurnAnnouncment(player1);
-                GameTurn(map, out int xChoice, out int yChoice);
-                map[xChoice - 1, yChoice - 1] = 1;
-                Mapgrid(map);
-                Console.WriteLine();
-                if (WinRow(map) || WinColumn(map) || WinDiagonal(map) || MapFull(map))
-                {
-                    break;
-                }
-                else
-                {
-                    //Turn player 2
-                    PrintTurnAnnouncment(player2);
-                    GameTurn(map, out xChoice, out yChoice);
-                    map[xChoice - 1, yChoice - 1] = 2;
-                    Mapgrid(map);
-                    Console.WriteLine();
-                }
+                PlayerTurn(gameBoard, map, player1, 1);
+                if (gameBoard.WinGame(map)) break;
+                else PlayerTurn(gameBoard, map, player2, 2);
             }
+            if (gameBoard.WinGame(map))
+            {
+                winner = gameBoard.CheckWhoWins(map, player1, player2);
+                PrintVictoryLine(winner);
 
-            //check who won
-            if (WinRow(map))
-            {
-                if ((map[0, 0] == 1 && map[0, 1] == 1 && map[0, 2] == 1) ||
-                    (map[1, 0] == 1 && map[1, 1] == 1 && map[1, 2] == 1) ||
-                    (map[2, 0] == 1 && map[2, 1] == 1 && map[2, 2] == 1))
-                {
-                    PrintVictoryLine(player1);
-                }
-                else
-                {
-                    PrintVictoryLine(player2);
-                }
             }
-            if (WinColumn(map))
-            {
-                if ((map[0, 0] == 1 && map[1, 0] == 1 && map[2, 0] == 1) ||
-                    (map[0, 1] == 1 && map[1, 1] == 1 && map[2, 1] == 1) ||
-                    (map[0, 2] == 1) && map[1, 2] == 1 && map[2, 2] == 1)
-                {
-                    PrintVictoryLine(player1);
-                }
-                else
-                {
-                    PrintVictoryLine(player2);
-                }
-            }
-            if (WinDiagonal(map))
-            {
-                if (map[1, 1] == 1)
-                {
-                    PrintVictoryLine(player1);
-                }
-                else
-                {
-                    PrintVictoryLine(player2);
-                }
-            }
-            if (MapFull(map) && !WinRow(map) && !WinColumn(map) && !WinDiagonal(map))
+            if (gameBoard.MapFull(map))
             {
                 Console.WriteLine("Map is full. Let's call it a draw");
             }
-
             Console.WriteLine("\nPress <ENTER> to exit!");
             Console.ReadLine();
         }
 
         //announces the player whoms turn it is
-        private static void PrintTurnAnnouncment(string player)
+        static void PrintTurnAnnouncment(string player)
         {
             Console.WriteLine("{0}. It is your turn!", player);
         }
 
         //intro tot the game
-        private static void Intro(out string player1, out string player2)
+        static void Intro(out string player1, out string player2)
         {
             Console.WriteLine("*** Tic Tac Toe ***");
             Console.WriteLine("Welcome to the game Tic Tac Toe!\n");
@@ -104,7 +55,7 @@ namespace TicTacToe
         }
 
         //method player
-        public static string Player(int playerNumber)
+        static string Player(int playerNumber)
         {
             Console.WriteLine("Player {0} please enter your name: ", playerNumber);
             string player = Console.ReadLine();
@@ -120,7 +71,7 @@ namespace TicTacToe
         }
 
         // method for players turn
-        private static void GameTurn(int[,] map, out int xChoice, out int yChoice)
+        static void GameTurn(int[,] map, out int xChoice, out int yChoice)
         {
             do
             {
@@ -145,97 +96,19 @@ namespace TicTacToe
             } while (map[xChoice - 1, yChoice - 1] != 0);
         }
 
-        // method for calling the grid
-        public static void Mapgrid(int[,] map)
+        static void PlayerTurn(GameBoard gameBoard, int [,] map, string player, int playerNumber)
         {
-            Console.WriteLine(" 123");
-            for (int i = 0; i < map.GetLength(0); i++)
-            {
-                Console.Write(i + 1);
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    if (map[j, i] == 0)
-                    {
-                        Console.Write(" ");
-                    }
-                    if (map[j, i] == 1)
-                    {
-                        Console.Write("X");
-                    }
-                    if (map[j, i] == 2)
-                    {
-                        Console.Write("O");
-                    }
-                }
-                Console.WriteLine();
-            }
+            PrintTurnAnnouncment(player);
+            GameTurn(map, out int xChoice, out int yChoice);
+            map[xChoice - 1, yChoice - 1] = playerNumber;
+            gameBoard.PrintMap(map);
+            Console.WriteLine();
         }
-
-        // win check methods for three in a row, column and diagonally
-            public static bool WinRow(int[,] map)
-            {
-                bool ret = false;
-                for (int i = 0; i < map.GetLength(0); i++)
-                {
-                    if (map[i, 0] != 0)
-                    {
-                        if (map[i, 0] == map[i, 1] && map[i, 1] == map[i, 2])
-                            ret = true;
-                    }
-                }                        
-                return ret;
-            }
-
-            public static bool CheckThreeInColumn(int [,] map, int column)
-            {
-                return (map[0, column] != 0 && map[0, column] == map[1, column] && map[1, column] == map[2, column]);
-            }
-
-            public static bool WinColumn(int [,] map)
-            {
-            return (CheckThreeInColumn(map, 0) ||
-                 CheckThreeInColumn(map, 1) ||
-                 CheckThreeInColumn(map, 2));
-            }
-
-            public static bool WinDiagonal(int[,] map)
-            {
-                bool ret = false;
-                if ((map[1, 1] != 0 && map[0, 0] == map[1, 1] && map[1, 1] == map[2, 2]) ||
-                    (map[1, 1] != 0 && map[0, 2] == map[1, 1] && map[1, 1] == map[2, 0]))
-                {
-                    ret = true;
-                }
-                return ret;
-            }
-
-            //method for check if field is full
-            public static bool MapFull(int[,] map)
-            {
-                bool ret = false;
-                int emptySpotCount = 0;
-                for (int i = 0; i < map.GetLength(0); i++)
-                {
-                    for (int j = 0; j < map.GetLength(1); j++)
-                    {
-                        if (map[i, j] == 0)
-                        {
-                            emptySpotCount++;
-                        }
-                    }
-                }
-                if (emptySpotCount == 0)
-                {
-                    ret = true;
-                }
-                return ret;
-            }
-
             //method for printing the victory line
-            public static void PrintVictoryLine(string player)
-            {
-                Console.WriteLine("Well done {0}, YOU WON!", player);
-            }
+        static void PrintVictoryLine(string player)
+        {
+            Console.WriteLine("Well done {0}, YOU WON!", player);
+        }
     }
 }
 
